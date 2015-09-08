8eb4980d844f66d187f9dda129969f311b09f6ba
Alex Wilde <shreddinmachine@gmail.com>
2015-09-08 00:44:06 -0400

    Massive internal improvements.
    100% internal CSS, remove Google analytics, compress site, add
    horizontal ruler between contents and credits.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ F32site
 Site for Faithful 32 Modded Edition
 
 Pull request are welcome but please don't overly bloat the site.
+
+Rename .php file to index.php for usage or testing.
--- no-comments-uncompressed.php
+++ no-comments-uncompressed.php
@@ -8,67 +8,81 @@
     <meta name="keywords" content="faithful, faithful mod support, faithful 32, faithful modded, f32, f32me, f32 modded edition, faithful 32 modded edition, faithful 32 me">
     <meta name="robots" content="index, follow">
     <title>Faithful 32 - Modded Edition</title>
-    <link href="Style1.css" rel="stylesheet">
     <link rel="shortcut icon" type="image/ico" href="favicon1.png">
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-49976593-1']);
-        _gaq.push(['_trackPageview']);
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
+	<style>
+	a {
+		text-decoration: none;
+		color: #6A75E8;
+	}
+		a:hover {
+		color: #9098EA;
+	}
+	#logo {
+		text-align: center;
+		text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.75);
+	}
+	.button {
+		float: left;
+		text-align: center;
+	}
+	.button a {
+		display: block;
+		padding: 7px 10px;
+		border-radius: 5px;
+		border: 0px;
+		color: #222222 !important;
+		text-decoration: none;
+		margin: 7px;
+		background-color: #fafafa;
+		box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.75);
+	}
+	.button a:hover {
+		background-color: #c0c0c0;
+	}
+	#text {
+		padding: 7px 10px;
+	}
+	.clear {
+		clear: both;
+	}
+	</style>
 </head>
-<body>
-<!-- Logo -->
+<body bgcolor="#262626" style="font-size:100%;color:#FFFFFF;padding-left:10%;padding-right:10%;">
+<font face="'Open Sans', verdana, sans-serif">
 <h1 id="logo">Faithful 32 <span style="color: #0026FF">Modd</span><span style="color: #32A800">ed Edi</span><span style="color: #E50000">tion</span></h1>
 
-<!-- 1.7.10 DL -->
-<div class="button button-50">
+<div class="button" style="width:50%;">
     <a href="http://www.f32.me/F32-1.7.10.zip">
         <span style="font-weight: bold;">Download</span> - Latest Pack - <span style="font-weight: bold;">(1.7.10)</span>
 		<br>
-        <!-- Fetch and display last time file was modified. -->
-        <?php
-        // Output example: Last rebuilt: March 27 2015 22:16:23.
-        echo "Last rebuilt: ".date("F d Y H:i",@filemtime("F32-1.7.10.zip"));
-        ?>
+        <?php echo "Last rebuilt: ".date("F d Y H:i",@filemtime("F32-1.7.10.zip")); ?>
+        
     </a>
 </div>
 
-<!-- 1.6.4 DL -->
-<div class="button button-50">
+<div class="button" style="width:50%;">
     <a href="http://www.f32.me/F32-1.6.4.zip">
         <span style="font-weight: bold;">Download</span> - Latest Pack - <span style="font-weight: bold;">(1.6.4)</span>
 		<br>
-        <!-- Fetch and display last time file was modified. -->
-        <?php
-        // Output example: Last rebuilt: March 27 2015 22:16:23.
-        echo "Last rebuilt: ".date("F d Y H:i",@filemtime("F32-1.6.4.zip"));
-        ?>
+        <?php echo "Last rebuilt: ".date("F d Y H:i",@filemtime("F32-1.6.4.zip")); ?>
     </a>
 </div>
 
 <br class="clear">
 
-<!-- 1.5.2 & 1.4.7 DL -->
-<div class="button button-100">
+<div class="button" style="width:100%;">
     <a href="http://www.f32.me/old">
         Older Packs (1.5.2 & 1.4.7)
     </a>
 </div>
 
-<!-- 1.7.10 Mod Support -->
-<div class="button button-50">
+<div class="button" style="width:50%;">
     <a href="http://www.f32.me/1710">
         Individual Mod Support (1.7.10)
     </a>
 </div>
 
-<!-- 1.6.4 Mod Support -->
-<div class="button button-50">
+<div class="button" style="width:50%;">
     <a href="http://www.f32.me/164">
         Individual Mod Support (1.6.4)
     </a>
@@ -81,7 +95,8 @@
         Assistance, comments, concerns, and issues on Github - <a href="https://github.com/F32Organization/Faithful32-1.7.10">(1.7.10)</a>
         <a href="https://github.com/F32Organization/Faithful32-1.6.4">(1.6.4)</a>
     </p>
-    <p id="credits">
+<hr>	
+    <p id="credits" style="font-size:100%;">
         Credit:
 		<br>	
 		Hosting provided by Greenphlem.
@@ -99,5 +114,6 @@
         </a>
     </p>
 </div>
+</font>
 </body>
 </html>
